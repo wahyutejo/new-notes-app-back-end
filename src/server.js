ac96fd54f74e1336ae3ee1bdd4448a54847f3197
wahyutejo <wahyut88@gmail.com>
2023-01-25 17:04:00 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const UserService = require('./services/postgres/UsersService');
+const UserValidator = require('./validator/users');
+const users = require('./api/users');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UserService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,13 +20,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UserValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
